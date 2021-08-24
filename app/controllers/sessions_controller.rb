74b9eb5bcf1d39e998e8c8234761dc2640f37640
Catherine Hu <catherinehu_9898@hotmail.com>
2021-08-24 01:40:48 +0000

    added user spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,4 +17,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil 
     redirect_to '/login'
   end
+
+  
+def create
+  if user = User.authenticate_with_credentials(params[:email], params[:password])
+    session[:user_id] = user.id
+    redirect_to '/'
+  else
+    redirect to '/login'
+  end
+end
+
 end
---
+++ spec/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+Rspec.describe User, type: :model do
+  describe "Validations" do
+    it "is valid with matching user and pw" do
+      @user = User.new(name: "tester", email: "test@test.com", password: "fiveletters", password_confirmation: "fiveletters")
+
+      expect(@user).to be_valid
+    end
+
+    it "is not valid if pw and pw confirmation do not match" do
+      @user = User.new(name: "tester", email: "test@test.com", password: "fiveletters", password_confirmation: "sixletters")
+
+      expect(@user).to_not be_valid
+    end 
+
+    it "is not valid if email is not unique" do
+      @user = User.new(name: "tester", email: "test@test.com", password: "fiveletters", password_confirmation: "fiveletters")
+      @user2 = User.new(name: "tester2", email: "TEST@TEST.com", password: "sixletters", password_confirmation: "sixletters")
+      
+      @user.save!
+      expect(@user).to be_valid
+      expect(@user2).to_not be_valid
+    end
+
+    it "not valid without email, first name" do
+      @user = User.new(email: "test@test.com", password: "fiveletters", password_confirmation: "fiveletters")
+      @user2 = User.new(name: "tester", password: "fiveletters", password_confirmation: "fiveletters")
+
+      expect(@user).to_not be_valid
+      expect(@user2).to_not be_valid
+    end
+
+    it "is not valid if password length shorter than required" do
+      @user = User.new(name: "tester", password: "1", password_confirmation: "1")
+
+      expect(@user).to_not be_valid
+    end
+  end
+
+    describe '.authenticate_with_credentials' do
+      it 'can login with registered email and password' do
+        @user = User.create(name: "tester", email: "test@test.com", password:"fiveletters", password_confirmation:"fiveletters")
+        expect(User.authenticate_with_credentials("test@test.com", "fiveletters")).to eq(@user)
+      end
+end
