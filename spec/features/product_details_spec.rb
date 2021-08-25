c7b0eb114dd20a6b9ce068ca8fa0a84f5694becf
Catherine Hu <catherinehu_9898@hotmail.com>
2021-08-25 00:38:50 +0000

    added feature spec: product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  # SETUP
+RSpec.feature "Visitor clicks on an item and is redirected to the product page", type: :feature, js: true do
+
   before :each do
     @category = Category.create! name: 'Apparel'
 
     10.times do |n|
       @category.products.create!(
-        name: Faker::Hipster.sentence(3),
+        name: Faker::Hipster.sentence(5),
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
-      )      
+      )
     end
   end
 
-  scenario "They see all products" do
-    # ACT
+  scenario "They click on the first product" do
     visit root_path
-
-    # DEBUG / VERIFY
-    # save_screenshot
-
+    first(".btn-default").click
+    
     expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css '.product-detail'
+    save_screenshot
   end
 
 end
